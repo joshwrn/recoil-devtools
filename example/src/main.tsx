6778050b249b51d030d5616dcadbadc8b7eec972
Josh Warren <joshnwarren@gmail.com>
2022-08-31 19:41:37 -0700

    update example

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -8,25 +8,35 @@ import "./index.css"
 import RecoilInspector from "../../src/App"
 import Bg from "./bg.jpg"
 import { fakeState, fakeState2, fakeState3 } from "./fakeState"
+import styled from "styled-components"
+
+const Container = styled.div`
+  display: flex;
+  width: 100vw;
+  height: 100vh;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`
 
 const Example: FC = () => {
   useRecoilValue(fakeState)
   useRecoilValue(fakeState2)
   useRecoilValue(fakeState3)
   return (
-    <div>
-      <img src={Bg} style={{ width: `100vw`, height: `100vh` }} />
-    </div>
+    <Container>
+      <RecoilInspector />
+      <img src={Bg} />
+    </Container>
   )
 }
 
 ReactDOM.createRoot(document.getElementById(`root`) as HTMLElement).render(
   <RecoilRoot>
     <React.StrictMode>
-      <div>
-        <Example />
-        <RecoilInspector />
-      </div>
+      <Example />
     </React.StrictMode>
   </RecoilRoot>
 )
