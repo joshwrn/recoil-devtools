b020aa74124775e0af4c53f007bf5b2a89a185df
Josh Warren <joshnwarren@gmail.com>
2022-08-31 20:09:51 -0700

    imports

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,5 +1,5 @@
 import type { FC } from "react"
-import { Fragment, useEffect, useRef } from "react"
+import React, { Fragment, useEffect, useRef } from "react"
 
 import { AnimatePresence, motion } from "framer-motion"
 import type { RecoilValue, Snapshot } from "recoil"
